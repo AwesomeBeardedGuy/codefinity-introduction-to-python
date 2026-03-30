66dada873870e8ad31fcf459f44b76602d72d197
AwesomeBeardedGuy <>
2026-03-30 22:31:17 +0000

    Variable Assignment

---
+++ variables_and_types/variable_assignment/main.py
@@ -0,0 +1,11 @@
+# 1) Define all variables first
+item_name = "Banana"
+item_price = 0.50
+item_stock_quantity = 100   # fixed spelling
+in_stock = True
+
+# 2) Then print them
+print("Item : ", item_name)
+print("Price per unit: $", item_price)
+print("quantity in stock: ", item_stock_quantity)
+print("In stock: ", in_stock)
